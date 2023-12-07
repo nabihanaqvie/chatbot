5a96dca958c2ba320f64aa3127a4714907226272
Sarah Hoover <shoover@ischool.berkeley.edu>
2023-12-07 09:42:35 -0800

    replaced sagemaker endpoint with huggingface pipeline

--- app_w210.py
+++ app_w210.py
@@ -5,6 +5,7 @@ import json
 import os
 from typing import List
 from sentence_transformers import SentenceTransformer
+from transformers import pipeline
 import torch
 import numpy as np
 import pinecone
@@ -16,7 +17,8 @@ import pandas as pd
 
 
 
-endpoint_name = "huggingface-pytorch-tgi-inference-2023-11-21-00-38-12-570" 
+#endpoint_name = "huggingface-pytorch-tgi-inference-2023-11-21-00-38-12-570"
+dialog_studio_pipe = pipeline("text2text-generation", model="Himabindu/finetuned-t5-dialogstudio-npc", max_new_tokens=250)
 st.sidebar.image("berkeley1.png", width=300)
 st.sidebar.markdown("""<div style="text-align:center;">""", unsafe_allow_html=True) 
 st.sidebar.title("W210 - Capstone")
@@ -119,22 +121,26 @@ def get_text():
     input_text = st.text_input("You: ","Hello", key="input")
     return input_text 
 
-def query(payload):
-    runtime = session.client('runtime.sagemaker')
-    response = runtime.invoke_endpoint(
-        EndpointName=endpoint_name,
-        ContentType='application/json',
-        Body=payload)
-    result = json.loads(response['Body'].read().decode())
-    return result
+#def query(payload):
+    #runtime = session.client('runtime.sagemaker')
+    #response = runtime.invoke_endpoint(
+        #EndpointName=endpoint_name,
+        #ContentType='application/json',
+        #Body=payload)
+    #result = json.loads(response['Body'].read().decode())
+    #return result
 
-def generate_response(prompt):
-    payload = {
-        "inputs": prompt
-    }
+#def generate_response(prompt):
+    #payload = {
+        #"inputs": prompt
+    #}
 
-    response = query(json.dumps(payload))
-    return response[0]["generated_text"]
+    #response = query(json.dumps(payload))
+    #return response[0]["generated_text"]
+    
+def generate_dialog_studio_response(input):
+    return dialog_studio_pipe(input)[0]['generated_text']
+
 
 def get_previous_responses(user_input):
 
@@ -158,7 +164,7 @@ character_name = list(character_level_dataset[character_level_dataset['name'] ==
 if user_input:
     previous_res = get_previous_responses(user_input)
     prompt = bio + " " + previous_res + " " + user_input
-    output = generate_response(prompt)
+    output = generate_dialog_studio_response(prompt)
     st.session_state.past.append(("You", user_input))
     st.session_state.generated.append((character, output))
 
@@ -168,5 +174,3 @@ for i in range(len(st.session_state['past']) - 1, -1, -1):
     character_name, response = st.session_state['generated'][i]
     st.write(f"{msg_type}: {msg}")
     st.write(f"{character_name}: {response}")
-
-

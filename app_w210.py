4e3c4413ca4089403f89b05f3df5eb6b556bdb67
Sarah Hoover <shoover@ischool.berkeley.edu>
2023-12-12 15:36:11 -0800

    option to create new character

--- app_w210.py
+++ app_w210.py
@@ -36,25 +36,78 @@ st.sidebar.markdown("""
 """, unsafe_allow_html=True)
 st.sidebar.markdown("""---""")
 
-# Import the dataset to get the character names/bios
-npc_train = load_dataset("amaydle/npc-dialogue", split="train")
-npc_test = load_dataset("amaydle/npc-dialogue", split="test")
 
-# Automatically splits it into train and test for you - let's ignore that for now and just combine them as one
+def get_text():
+    input_text = st.text_input("You: ","Hello", key="input")
+    return input_text
 
-# First, transform them into pandas DFs
-train = pd.DataFrame(data = {'name': npc_train['Name'], 'bio':npc_train['Biography'], 'query':npc_train['Query'], 'response':npc_train['Response'], 'emotion':npc_train['Emotion']})
-test = pd.DataFrame(data = {'name': npc_test['Name'], 'bio':npc_test['Biography'], 'query':npc_test['Query'], 'response':npc_test['Response'], 'emotion':npc_test['Emotion']})
+# Radio button - make your own character or choose from existing characters
+choice = st.radio(
+    "What would you like to do?",
+    ["Create your own character", "Choose an existing character"])
 
-# Now combine into a single df
-npc = pd.concat([train, test])
 
-# Create a character-level dataset, since the characters show up multiple times in the dataset
-character_level_dataset = npc[['name', 'bio']]
-character_level_dataset.drop_duplicates(inplace=True)
+if choice == 'Choose an existing character':
 
-# Get a list of just the names
-character_names = list(pd.unique(character_level_dataset['name']))
+    # Import the dataset to get the character names/bios
+    npc_train = load_dataset("amaydle/npc-dialogue", split="train")
+    npc_test = load_dataset("amaydle/npc-dialogue", split="test")
+
+    # Automatically splits it into train and test for you - let's ignore that for now and just combine them as one
+
+    # First, transform them into pandas DFs
+    train = pd.DataFrame(data = {'name': npc_train['Name'], 'bio':npc_train['Biography'], 'query':npc_train['Query'], 'response':npc_train['Response'], 'emotion':npc_train['Emotion']})
+    test = pd.DataFrame(data = {'name': npc_test['Name'], 'bio':npc_test['Biography'], 'query':npc_test['Query'], 'response':npc_test['Response'], 'emotion':npc_test['Emotion']})
+
+    # Now combine into a single df
+    npc = pd.concat([train, test])
+
+    # Create a character-level dataset, since the characters show up multiple times in the dataset
+    character_level_dataset = npc[['name', 'bio']]
+    character_level_dataset.drop_duplicates(inplace=True)
+
+    # Get a list of just the names
+    character_names = list(pd.unique(character_level_dataset['name']))
+    
+    col1, col2 = st.columns([1, 5])
+    with col1:
+        st.markdown("""
+            <img src="https://media4.giphy.com/media/13xxoHrXk4Rrdm/giphy.gif?cid=ecf05e47sr7dxi5kpveq2tcml2i43065zsgyyf9fkmual0l7&ep=v1_stickers_search&rid=giphy.gif&ct=s" width="150">
+        """, unsafe_allow_html=True)
+
+    with col2:
+        st.markdown("<div style='padding-top:30px'></div>", unsafe_allow_html=True)
+        st.title("Team NPC")
+        character = st.selectbox("Choose a character",
+                             character_names)
+                             
+    # Once the character is selected, get the bio
+    bio = list(character_level_dataset[character_level_dataset['name'] == character]['bio'])[0]
+
+else:
+    def create_character_name():
+        input_text = st.text_input("Character name: ","Spongebob Squarepants", key="new_character")
+        return input_text
+        
+    def create_bio():
+        input_text = st.text_area("Bio: ", "A square yellow sponge named SpongeBob SquarePants lives in a pineapple with his pet snail, Gary, in the city of Bikini Bottom on the floor of the Pacific Ocean. He works as a fry cook at the Krusty Krab. During his time off, SpongeBob has a knack for attracting trouble with his starfish best friend, Patrick. Arrogant octopus Squidward Tentacles, SpongeBob’s neighbor, dislikes SpongeBob because of his childlike behavior.", key="new_bio")
+        return input_text
+    
+    col1, col2 = st.columns([1, 5])
+    with col1:
+        st.markdown("""
+            <img src="https://media4.giphy.com/media/13xxoHrXk4Rrdm/giphy.gif?cid=ecf05e47sr7dxi5kpveq2tcml2i43065zsgyyf9fkmual0l7&ep=v1_stickers_search&rid=giphy.gif&ct=s" width="150">
+        """, unsafe_allow_html=True)
+
+    with col2:
+        st.markdown("<div style='padding-top:30px'></div>", unsafe_allow_html=True)
+        st.title("Team NPC")
+        character = create_character_name()
+        bio = create_bio()
+
+# Input bar at the bottom
+user_input = get_text()
+
 
 # Set up the sentence encoder for RAG
 rag_encoder = SentenceTransformer('sentence-transformers/all-MiniLM-L6-v2')
@@ -74,24 +127,8 @@ pinecone.init(
 )
 index_name='npc-rag'
 index = pinecone.Index(index_name)
-
-
-col1, col2 = st.columns([1, 5])
-with col1:
-    st.markdown("""
-        <img src="https://media4.giphy.com/media/13xxoHrXk4Rrdm/giphy.gif?cid=ecf05e47sr7dxi5kpveq2tcml2i43065zsgyyf9fkmual0l7&ep=v1_stickers_search&rid=giphy.gif&ct=s" width="150">
-    """, unsafe_allow_html=True)
-
-with col2:
-    st.markdown("<div style='padding-top:30px'></div>", unsafe_allow_html=True)
-    st.title("Team NPC")
-    character = st.selectbox("Choose a character", 
-                         character_names)
                          
-                         
-# Once the character is selected, get the bio
-bio = list(character_level_dataset[character_level_dataset['name'] == character]['bio'])[0]
-
+                        
 key = os.environ.get('key')
 secret  = os.environ.get('secret')
 
@@ -111,11 +148,6 @@ if character != st.session_state.get("current_character"):
     st.session_state['generated'] = []
     st.session_state['past'] = []
     st.session_state["current_character"] = character
-
-
-def get_text(): 
-    input_text = st.text_input("You: ","Hello", key="input")
-    return input_text 
 
 def query(payload, endpoint_name):
     runtime = session.client('runtime.sagemaker')
@@ -173,13 +205,6 @@ def get_bio_responses(query_vec):
     b = np.array(bio_vec)
     cosine_similarity = np.dot(a,b)/(norm(a)*norm(b))
     return cosine_similarity
-
-
-# Input bar at the bottom
-user_input = get_text()
-
-# Extract character's name from the bio outside of the if block
-character_name = list(character_level_dataset[character_level_dataset['name'] == character]['name'])[0]
 
 
 if user_input:
